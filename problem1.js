38c237c685e9674bcb15aae2b1f5707a54755905
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-08 21:55:07 +1100

    problem 1

--- problem1.js
+++ problem1.js
@@ -4,27 +4,9 @@ function findStudent(students, studentName){
 
     if(students.length <=0 ) return -1
 
-    // for(let i = 0; i < students.length; i++){
-    //     if(students[i] === studentName){
-    //         return i;
-    //     }
-    // }
-
-    let start = 0;
-    let end = students.length - 1
-
-    while (start <= end){
-
-        let mid = Math.floor((start + end) / 2)
-
-        if(students[mid] === studentName){
-            return mid
-        }
-        else if(students[mid] > studentName){
-            end = mid - 1
-        }
-        else{
-            start = mid + 1
+    for(let i = 0; i < students.length; i++){
+        if(students[i] === studentName){
+            return i;
         }
     }
 }
