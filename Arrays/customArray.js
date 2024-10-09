74391fa50e3e5017103708b0581c81ab57fdefd5
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-09 13:18:34 +1100

    made custom array

---
+++ Arrays/customArray.js
@@ -0,0 +1,59 @@
+class customArray{
+    constructor(){
+        this._length = 0
+        this._data = {}
+    }
+
+    // length method
+    length(){
+        return this._length
+    }
+
+    // push method
+    push(item){
+        this._data[this._length] = item
+        this._length++
+    }
+
+    // get method
+    get(index){
+        return this._data[index]
+    }
+
+    // pop method
+    pop(){
+        delete this._data[this._length - 1]
+        this._length--
+    }
+
+    shift(){
+        
+        for(let i = 0; i < this._length; i++){
+            this._data[i] = this._data[i + 1]
+        }
+
+        delete this._data[this._length - 1]
+        this._length--
+
+    }
+
+}
+
+const myArray = new customArray()
+
+myArray.push("hello")
+myArray.push("world")
+myArray.push("this")
+myArray.push("is")
+myArray.push("custom")
+myArray.push("array")
+
+
+console.log(myArray);
+myArray.shift()
+console.log(myArray);
+
+
+
+
+
