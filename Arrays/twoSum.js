aade385e9229d56430dcfecd6e306becfb82a6a1
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-13 23:31:43 +1100

    two sum

--- Arrays/twoSum.js
+++ Arrays/twoSum.js
@@ -1,4 +1,4 @@
-// Two Sum
+// Two Sum Ugly Code
 
 const twoSum = (nums, target) => {
 
@@ -9,6 +9,22 @@ const twoSum = (nums, target) => {
             }
         }
     }
+
+
+    // let map = {}
+
+    // for(let i = 0; i < nums.length; i++){
+        
+    //     let complement = target - nums[i]
+
+    //     if(map.hasOwnProperty(complement)){
+    //         return [map[complement], i]    
+    //     }
+
+    //     map[nums[i]] = i
+    // }
+
+    
 }
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 7, 11, 15, 18, 39], 18));
