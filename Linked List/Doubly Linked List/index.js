886a3f17b8bf496560304adf22e7be18a083bb40
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-15 23:16:23 +1100

    doubly linkedlist and push method

---
+++ Linked List/Doubly Linked List/index.js
@@ -0,0 +1,34 @@
+class Node{
+    constructor(value){
+        this.value = value
+        this.next = null
+        this.prev = null
+    }
+}
+
+
+class DoublyLinkedList{
+    constructor(value){
+        let newNode = new Node(value)
+        this.head = newNode
+        this.tail = newNode
+        this.length = 1
+    }
+
+
+    // push method
+    push(value){
+        let newNode = new Node(value)
+
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }
+
+        this.tail.next = newNode
+        newNode.prev = this.tail
+        this.tail = newNode
+
+        this.length++
+    }
+}
