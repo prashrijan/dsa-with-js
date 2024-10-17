05477a40ada731511ea90498533a39cd36091695
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-17 13:11:40 +1100

    stack push method

--- Stack/index.js
+++ Stack/index.js
@@ -12,9 +12,26 @@ class Stack{
         this.first = newNode
         this.length = 1;
     }
+
+    push(value){
+
+        let newNode = new Node(value)
+
+        if(!this.first){
+            this.first = newNode
+        }
+
+        newNode.next = this.first
+        this.first = newNode
+
+        this.length++
+    }
 }
 
 
 let myStack = new Stack(1)
+myStack.push(2)
+myStack.push(3)
+
 
 console.log(myStack);
