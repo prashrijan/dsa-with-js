3d6243974688d07c9508d03332c8c724f6cd4b5e
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-21 22:24:44 +1100

    debugging

--- HashTables/index.js
+++ HashTables/index.js
@@ -9,7 +9,7 @@ class HashTable{
         const PRIME_NUMBER = 31
 
         for(let i = 0; i < Math.min(key.length, 100); i++){
-            const charCode = key.charCodeAt(0) - 96
+            const charCode = key.charCodeAt(i) - 96
 
             sum = (sum * PRIME_NUMBER + charCode) % this.keyMap.length
         }
@@ -20,13 +20,19 @@ class HashTable{
     set(key, value){
         const index = this._hashFunction(key)
 
-        if(!this.keyMap[index]) this.keyMap = []
+        if(!this.keyMap[index]) this.keyMap[index] = []
 
-        this.keyMap[index].push(key, value)
+        this.keyMap[index].push([key, value])
         return this
+    }
+
+    get(){
+
     }
 }
 
 const phoneBook = new HashTable()
 
 phoneBook.set('john', '0450528184')
+
+console.log(phoneBook);
