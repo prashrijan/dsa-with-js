8eefcccbd4b2e4c877c09c561abdbc459fb2bcc4
Prashrijan <shresthaprashrijan@gmail.com>
2024-10-22 14:56:02 +1100

    bst insert method

--- Tree/index.js
+++ Tree/index.js
@@ -19,12 +19,39 @@ class BST{
             this.root = newNode
         }
 
+        let temp = this.root
 
+        while(true){
+            if(this.root.value === newNode.value){
+                return undefined
+            }
+
+            if(newNode.value < temp.value){
+                if(temp.left === null){
+                    temp.left = newNode
+                    return this
+                }else{
+                    temp = temp.left
+                }
+            }else{
+                if(temp.right === null){
+                    temp.right = newNode
+                    return this
+                }else{
+                    temp = temp.right
+                }
+            }
+        }
     }
 }
 
 let myTree = new BST()
 
 myTree.insert(5)
+myTree.insert(6)
+myTree.insert(7)
+myTree.insert(1)
+myTree.insert(2)
+myTree.insert(3)
 
 console.log(myTree);
